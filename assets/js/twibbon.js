673d97173db7db689253941fcc67f3811f605ab6
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2022-10-29 00:14:09 +0700

    Update twibbon.js

--- assets/js/twibbon.js
+++ assets/js/twibbon.js
@@ -42,7 +42,7 @@ function uploadImage(_this) {
   var fileType = file["type"];
   var validImageTypes = ["image/gif", "image/jpeg", "image/png"];
   if ($.inArray(fileType, validImageTypes) < 0) {
-       alert('Please select an image! Fill is not an image')
+       alert('<div class="alert alert-success">Please select an image! Fill is not an image!</div>')
        _this.form.reset()
   } else {
     alert('All good')
