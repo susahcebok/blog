44bbd71b903840c686b51656fd352cd42d20f021
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2022-10-29 12:16:58 +0700

    Create qr.js

---
+++ assets/js/qr.js
@@ -0,0 +1,7 @@
+document.querySelector("#submit").addEventListener("click", (event) => {
+  const uri =
+    "https://chart.googleapis.com/chart?cht=qr&choe=UTF-8&chs=350x350&chl=";
+  event.preventDefault();
+  url = uri + document.querySelector("#url").value;
+  window.location.href = url;
+});
