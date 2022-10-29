75f5818755fed53ad030cbc3b92c4d4e07a09f2e
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2022-10-29 15:44:26 +0700

    Update qrcode.js

--- assets/js/qrcode.js
+++ assets/js/qrcode.js
@@ -1,8 +1,24 @@
-document.querySelector("#submit").addEventListener("click", (event) => {
-  const uri =
-    "https://chart.googleapis.com/chart?cht=qr&choe=UTF-8&chs=350x350&chl=";
-  event.preventDefault();
-  url = uri + document.querySelector("#url").value;
-  window.location.href = url;
+const mainContainer = document.querySelector(".container");
+const qrBtn = mainContainer.querySelector(".form button");
+const qrInput = mainContainer.querySelector(".form input");
+const qrImg = mainContainer.querySelector(".qr-code img");
+const clearBtn = mainContainer.querySelector('.form #clearBtn');
+
+clearBtn.addEventListener('click',() => {
+  if(!qrInput.value) return;
+  else {
+    qrInput.value = '';
+    mainContainer.classList.remove("active");
+  }
+})
+
+qrBtn.addEventListener("click", () => {
+  let _value = qrInput.value;
+  if (!_value) {
+    alert("Enter url or text!!!");
+  } else {
+    qrImg.src = `https://chart.googleapis.com/chart?cht=qr&choe=UTF-8&chs=350x350&chl=${_value}`;
+    mainContainer.classList.toggle("active");
+  }
 });
 
