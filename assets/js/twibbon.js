6d019fda1114cb303814224e4ca80ff091c7a0ec
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2022-10-30 22:38:28 +0700

    Update twibbon.js

--- assets/js/twibbon.js
+++ assets/js/twibbon.js
@@ -65,7 +65,8 @@ let enableBtnDownload = () => {
 let generate = () => {
     
     if(image.files[0] === undefined) 
-       alert("Gambar tidak boleh kosong!");
+    //   alert("Gambar tidak boleh kosong!");
+         alertify.error('Gambar tidak boleh kosong!');
 
     else{
         destroyInterval();                               
