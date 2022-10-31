dd1d7129f7f5611459c2579ddf7aa7f4d9f95080
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2022-10-31 22:35:40 +0700

    Update share.js

--- assets/js/share.js
+++ assets/js/share.js
@@ -9,9 +9,9 @@ const share = document.querySelector('.share');
 
 share.onclick = e => {
     navigator.share({
-        title: '{% if page.title %}{{ page.title }}{% else %}{{ site.name }} &mdash; {{ site.description }}{% endif %}',
-        text: '{% if page.description %}{{ page.description }}{% else %}{{ site.name }}{% endif %}',
-        url: '{{ page.title }}{{ page.shortUrl }}'
+        title: 'OHASTA.GA',
+        text: 'Just Another A Blog',
+        url: ''
     });
 };
 
