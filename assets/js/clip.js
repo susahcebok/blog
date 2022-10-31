dce5804b604a75c5dd13be3eac2da1ad22f89e39
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2022-10-31 23:31:54 +0700

    Update clip.js

--- assets/js/clip.js
+++ assets/js/clip.js
@@ -1,30 +1,5 @@
-function copyToClipboard(element) {
-	function clear() {
-		if (window.getSelection().empty) {
-			window.getSelection().empty();
-		} else if (window.getSelection().removeAllRanges) {
-			window.getSelection().removeAllRanges();
-		}
-	}
-
-	if (document.selection) {
-		var range = document.body.createTextRange();
-		range.moveToElementText(element);
-		range.select().createTextRange();
-		document.execCommand('Copy');
-		document.selection.empty();
-	} else if (window.getSelection) {
-		clear();
-		var range = document.createRange();
-		range.selectNode(element);
-		window.getSelection().addRange(range);
-		document.execCommand('Copy');
-		clear();
-	}
-}
-
-var textContainer = document.createElement('span');
-textContainer.innerText = window.location.href;
-document.body.appendChild(textContainer);
-copyToClipboard(textContainer);
-document.body.removeChild(textContainer);
+document.addEventListener('copy', function(event) {
+  var text = window.getSelection().toString() + " brought from http://example.com/test";
+  event.clipboardData.setData('text/html', text);
+  event.preventDefault();
+});
