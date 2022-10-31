efa03c1dba860819fbb5fa513e56d33228565734
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2022-10-31 22:51:32 +0700

    Update share.js

--- assets/js/share.js
+++ assets/js/share.js
@@ -1,4 +1,25 @@
-const button = document.getElementById('shared');
+     var shareButton = document.getElementById( "share" );
+            if( 'share' in navigator ) {
+                shareButton.addEventListener('click', share)
+                function share() {
+                    navigator.share({
+                        title: 'OHASTA.GA',
+                        text: 'Just Another A Blog',
+                        url: 'https://ohasta.ga/'
+                    })
+                    .then( () => {
+                        alert('Terima kasih telah berbagi!');
+                    })
+                    .catch( err => {
+                        alert('Mengapa Anda tidak membagikan saya?')
+                        console.log(`Rupanya ada yang tidak beres...``, err.message);
+                    })
+                }
+            } else {
+                alert('Browser Anda tidak didukung, coba dari perangkat seluler atau tablet!')
+            }
+
+// const button = document.getElementById('shared');
 button.addEventListener('click', function(evt){
   evt.preventDefault();
   if (navigator.share !== undefined) {
@@ -11,4 +32,4 @@ button.addEventListener('click', function(evt){
   } else {
     console.log('Your browser doesn\'t support Web Share API');
   }
-}, false)
+}, false) //
