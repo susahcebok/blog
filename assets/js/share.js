734c5421f0bb8cdfb1c3e8ddeddf9093524aa2cf
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2022-10-31 22:45:15 +0700

    Update share.js

--- assets/js/share.js
+++ assets/js/share.js
@@ -4,7 +4,7 @@ button.addEventListener('click', function(evt){
   if (navigator.share !== undefined) {
     navigator.share({
       title: 'OHASTA.GA - JUST ANOTHER A BLOG',
-      url  : '{{ site.url }}{{ post.url }}'
+      url  : 'https://ohasta.ga/'
     })
     .then(() => console.log('Shared successfully'))
     .catch((error) => console.log('An error has occurred'));
