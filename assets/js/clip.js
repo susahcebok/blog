c0aee444046e03b949d15304a8f974220917c94b
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2022-10-31 23:39:20 +0700

    Update clip.js

--- assets/js/clip.js
+++ assets/js/clip.js
@@ -1,5 +1,5 @@
 document.addEventListener('copy', function(event) {
-  var text = window.getSelection().toString() + " brought from http://example.com/test";
+  var text = window.getSelection().toString() + " Sumber dari https://ohasta.ga/";
   event.clipboardData.setData('text/html', text);
   event.preventDefault();
 });
