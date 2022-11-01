b822d66edf6aca028471a96045c22e3c06846b03
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2022-11-01 22:27:17 +0700

    Update love.js

--- assets/js/love.js
+++ assets/js/love.js
@@ -19,7 +19,7 @@ function lovecalculator(){
   var lovelevel = Math.random() * 100;
   lovelevel = Math.floor(lovelevel);
 //  alertify.success('Tingkat cintamu adalah' + ' ' + lovelevel + '%');
-  alertify.alert('Love Calculator', 'Tingkat cintamu adalah' + 'sname' + lovelevel + '%', function(){ alertify.success('Ok'); });
+  alertify.alert('Love Calculator', 'Tingkat cintamu adalah' + ' ' + lovelevel + '%', function(){ alertify.success('Ok'); });
 
   }
   
