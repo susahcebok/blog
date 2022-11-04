fff99392cdc6aa3e10e6825eb882d426a9272f74
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2022-11-04 09:23:21 +0700

    Update wm.js

--- assets/js/wm.js
+++ assets/js/wm.js
@@ -3,7 +3,7 @@ susahcebokCop = function(title,theFontColor) {
   var title,
       theFontColor;
   
-  var style = "@import url(https://fonts.googleapis.com/css?family=Quicksand:400,700);.copyright{position:fixed;right:15px;bottom:15px;font-family:Quicksand}.copyright span{line-height:30px;color:" + theFontColor +";margin-right:7.5px;float:left}.copyright span a{font-weight:400;text-decoration:none;color:#ea4c89}.copyright .susahcebok{width:24px;height:24px;display:block;background:url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/111167/balapa.jpg);background-size:cover;border-radius:50%;border:3px solid rgba(0,0,0,.25);float:right}";
+  var style = "@import url(https://fonts.googleapis.com/css?family=Quicksand:400,700);.copyright{position:fixed;right:15px;bottom:15px;font-family:Quicksand}.copyright span{line-height:30px;color:" + theFontColor +";margin-right:7.5px;float:left}.copyright span a{font-weight:400;text-decoration:none;color:#ea4c89}.copyright .susahcebok{width:24px;height:24px;display:block;background:url(https://avatars.githubusercontent.com/u/115649359?s=48&v=4);background-size:cover;border-radius:50%;border:3px solid rgba(0,0,0,.25);float:right}";
 
   var tag = "<span>" + title + " by</span><a class='susahcebok' href='#' target='_blank'></a>";
   
